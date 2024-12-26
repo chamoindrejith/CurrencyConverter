abcbe751e78565f1463831191e33cf15a196764b
Chamodi <chamodiindrejith14@gmail.com>
2024-12-26 21:07:40 +0530

    chore: Initial setup

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { PaperProvider,} from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+        <ScrollView>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <View style={styles.container}>
+            <StatusBar style="auto" />
+            <CurrencyConverter />
+          </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
